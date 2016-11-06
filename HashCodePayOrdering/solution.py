24be75df77320c743eda960b722a8d6f0d3d9205
Srdjan Slavnic <slavnic.srdjan@gmail.com>
2016-11-06 15:17:54 -0800

    Add explanation to solution.py

--- HashCodePayOrdering/solution.py
+++ HashCodePayOrdering/solution.py
@@ -3,12 +3,48 @@ from collections import defaultdict
 N = int(input("Maximum Pay: "))
 M = int(input("Number of employees: "))
 
+
+"""
+	Approach the problem as if it was a lattice path problem on:
+
+	--------------------------------------------------------  N
+	|														|
+	|														|
+	|														|
+	|														|  
+	|														|   pay
+	|														|
+	|														| 
+	|														|
+	|														|
+	|														| 1
+	--------------------------------------------------------  
+	1						rank							M
+
+
+	Notice that an invalid ordering in this model is any 3 successively decreasing
+	(left to right) or increasing (right to left) points. 
+
+	The number of valid paths at (m,n) will depend on the current pay n's relation
+	to two things:
+
+		1) The lowest pay thus far
+
+	If n > lowest pay thus far then paying any more would be an invalid ordering
+	so we're restricted to paying <= n in this case.
+
+		2) The highest pay thus far
+
+	If n < lowest pay thus far we can pay more for a lower rank, but must take
+	care not to pay greater than our previous max to avoid invalid orderings.
+"""
+
+
 def t():
 	return -1
 
 Ddict = defaultdict(t)
 	
-
 def DR(m, n, roll_min, roll_max):
 	if m == 1:
 		return 1
@@ -41,11 +77,13 @@ def DR(m, n, roll_min, roll_max):
 	Ddict[m, n, roll_min, roll_max] = sum
 	return sum
 
+
+
+#Sum over all starting points
 sum = 0
 for k in range(1, N + 1):
 	sum += DR(M, k, k, N)
 
-#sum = DR(M, N, N)
 print(sum)
 
 
